dabc48119ee45849e0b174821259a126cb473ce7
ds526-student <damionsklenarsclare@gmail.com>
2026-08-03 14:41:53 +0100

    init.sql updated to match basic schema

--- init.sql
+++ init.sql
@@ -13,3 +13,35 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_login TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS Products (
+    id uuid BIGINT PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    ingredients JSONB, -- cannot directly verify existance with jsonb gotta figure out a way to link properly mhm
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+)
+
+CREATE TABLE IF NOT EXISTS Ingredients (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    paper_url VARCHAR(255),
+    recommended_dosage VARCHAR(255),
+    maximum_dosage VARCHAR(255),
+    verified BOOLEAN DEFAULT FALSE,
+    image_url VARCHAR(255),
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS Data (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid REFERENCES users(id) ON DELETE CASCADE,
+    library_name VARCHAR(255) NOT NULL,
+    product_ids JSONB, -- cannot directly verify existance with jsonb gotta figure out a way to link properly mhm
+    image_url VARCHAR(255),
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
