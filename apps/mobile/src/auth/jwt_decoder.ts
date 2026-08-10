ccf10d69be2dfc2498abd9487bb8f90e396e8f2c
ds526-student <damionsklenarsclare@gmail.com>
2026-08-10 15:57:08 +0100

    session functions for keystore
    added functions and type to store session object in phones keystore for secure access/storage

--- apps/mobile/src/auth/jwt_decoder.ts
+++ apps/mobile/src/auth/jwt_decoder.ts
@@ -1,6 +1,12 @@
 import { jwtDecode } from 'jwt-decode';
 import type { AccessTokenResponse } from '@suppsense/shared-types';
 
+/**
+ * decodes the access token returning the data as an object
+ * this is done so we can retrieve the userId and userType since these are not included within the refresh token
+ * @param token the token to decode
+ * @returns the decoded token data
+ */
 export function decodeAccessToken(token: string): AccessTokenResponse {
     try {
         return jwtDecode<AccessTokenResponse>(token);
