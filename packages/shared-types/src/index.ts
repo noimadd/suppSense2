d13ca431e10d13fa5873e87dd94a88002040ffb4
ds526-student <damionsklenarsclare@gmail.com>
2026-08-10 15:08:51 +0100

    auth types added

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,3 +1,27 @@
+// Authentication related types
+export interface LoginRequest {
+    username: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    accessToken: string;
+    refreshToken: string;
+    sessionId: string;
+}
+
+export interface ApiErrorResponse {
+    message: string;
+}
+
+export interface AccessTokenResponse {
+    sub: string;
+    email: string;
+    userType: 'user' | 'admin';
+    sid: string;
+}
+
+
 export interface Supplement {
     id: string;
 }
