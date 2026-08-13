86645713495c615d9b074f81dd8e55b5f15e8b95
ds526-student <damionsklenarsclare@gmail.com>
2026-08-13 05:09:53 +0100

    ingredient getter

--- apps/backend/src/db/supplement.ts
+++ apps/backend/src/db/supplement.ts
@@ -34,3 +34,32 @@ export async function getProduct(barcode: string): Promise<Product | null> {
     );
     return result.rows[0] ?? null;
 }
+
+/**
+ * contains all ingredient info - matches the db ingredient schema
+ */
+export interface Ingredient {
+    id: string;
+    name: string;
+    description: string;
+    paper_url: string;
+    recommended_dosage: string;
+    maximum_dosage: string;
+    verified: boolean;
+    image_url: string;
+    date_added: string;
+    date_updated: string;
+}
+
+/**
+ * gets an ingredient from the db via it's name
+ * @param name of the ingredient
+ * @returns all ingredient information
+ */
+export async function getIngredient(name: string): Promise<Ingredient | null> {
+    const result = await pool.query<Ingredient>(
+        'SELECT * FROM ingredients WHERE name = $1',
+        [name]
+    );
+    return result.rows[0] ?? null;
+}
