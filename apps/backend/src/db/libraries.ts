b95e5f46dc42f86906262e3165e99d1039ea5739
Leo van der Merwe <4leovdm@gmail.com>
2026-08-26 16:10:47 +1200

    Moar library work

--- apps/backend/src/db/libraries.ts
+++ apps/backend/src/db/libraries.ts
@@ -60,3 +60,17 @@ export async function deleteProductLibrary(user_id: string, library_id: string):
 													[user_id, library_id]
 													);
 }
+
+/**
+* Inserts a new product entry into the given library.
+* @param user_id the id of the user who owns the library - There to protect against unauthorized insertion
+* @param library_id the id of the library to insert into 
+* @param product_id the id of the product we want to insert
+*/
+export async function addProductToProductLibrary(user_id: string, library_id: string, product_id: string): Promise<void>
+{
+	const result = await pool.query<ProductLibrary>(
+	'UPDATE librarydata SET product_ids = product_ids || (SELECT row_to_json(p_r) FROM (SELECT products.id AS product_id, products.name AS name FROM products WHERE id = $1) AS p_r)::jsonb WHERE librarydata.user_id = $2 AND librarydata.id = $3;',
+	[product_id, user_id, library_id]
+	);
+}
--- apps/backend/src/routes/libraries.routes.ts
+++ apps/backend/src/routes/libraries.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 //import { getUserLibrariesResponse } from '@suppsense/shared-types';
 import { decodeAccessToken, isValidResourceURL } from '../middleware/auth.middleware'
-import { getProductLibrary, createProductLibrary, deleteProductLibrary, ProductLibrary } from '../db/libraries'
+import { getProductLibrary, createProductLibrary, deleteProductLibrary, addProductToProductLibrary, ProductLibrary } from '../db/libraries'
 
 const router = Router();
 
@@ -94,6 +94,37 @@ router.delete('/delete', async (req, res) => {
     // verify ownership of the library because the deletion query does a combined check of both the user ID and the library ID when deleting.
     await deleteProductLibrary(token_decode.sub, library_id);
     
+    return res.json({message: 'Success'});
+});
+
+// Insert a product into a particular library
+router.post('/insert_product', async (req, res) => {
+    // We need to take the user's token and decode it to find their ID.
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    
+    if(!token)
+    {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    
+    const token_decode = decodeAccessToken(token);
+    if(!token_decode)
+    {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    
+    const { library_id, product_id } = req.body ?? {};
+    
+    if(!library_id || !product_id)
+    {
+        return res.status(401).json({ message: 'Invalid library or product id' });
+    }
+    
+    // Inser the product entry into the DB.
+    // Note(Leo): Insertion is protected via the JWT token encoding the user that it belongs to by their ID. We dont have to do a seperate query to 
+    // verify ownership of the library because the deletion query does a combined check of both the user ID and the library ID when deleting.
+    await addProductToProductLibrary(token_decode.sub, library_id, product_id);
+        
     return res.json({message: 'Success'});
 });
 
